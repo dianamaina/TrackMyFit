aaf2aea3e5c8c348e255ed4d911a56d10a2ce5db
diana maina <dianamainachui@gmail.com>
2020-10-23 14:04:09 -0400

    added starter files

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const ingredientsController = require("./controllers/ingredientsController");
+const pizzaController = require("./controllers/pizzaController")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/pizza-parlour",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+const connection = mongoose.connection;
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+app.use(ingredientsController);
+app.use(pizzaController);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
