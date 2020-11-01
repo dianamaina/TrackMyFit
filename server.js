790244bb3905e0e68a481d67b11e93bdce2c20dd
diana maina <dianamainachui@gmail.com>
2020-11-01 13:54:07 -0500

    changes made to server

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+const e = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+mongoose.connect
+(process.env.MONGODB_URI || "mongodb://locahost/trackMyFit",
+ { useNewUrlParser: true, useUnifiedTopology: true });
+
+ const connection = mongoose.connection;
+
+ connection.on("connected", () => {
+   console.log("Mongoose successfull connected.");
+ });
+
+ git add 
+
+
 
 app.get("/api/config", (req,res) => {
   res.json({
@@ -13,29 +33,5 @@ app.get("/api/config", (req,res) => {
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost${PORT}`);
 })
-app.use(express.json());
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/pizza-parlour",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
-const connection = mongoose.connection;
-connection.on("connected", () => {
-  console.log("Mongoose successfully connected.");
-});
-connection.on("error", (err) => {
-  console.log("Mongoose connection error: ", err);
-});
-app.get("/api/config", (req, res) => {
-  res.json({
-    success: true,
-  });
-});
-app.use(ingredientsController);
-app.use(pizzaController);
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+
